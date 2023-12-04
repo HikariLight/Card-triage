77638d61caf3489bd09418504461207ccfb9c3c7
Dhia M <merzougui.dhia@gmail.com>
2023-12-04 16:05:17 +0100

    Updated searchbar border color

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ const SearchBar = ({ setFilter }) => {
         <div className="my-2 flex justify-center">
             <input
                 type="text"
-                className=" w-3/4 appearance-none rounded-lg border border-gray-300 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
+                className="w-3/4 appearance-none rounded-lg border border-purple-400 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
                 placeholder="Filter"
                 onChange={(event) => {
                     setFilter(event.target.value)
