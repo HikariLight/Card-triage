467badfb4f6582912cc3ffab19a952e4e49b2316
Dhia M <merzougui.dhia@gmail.com>
2023-12-04 14:21:09 +0100

    Added border to column

--- frontend/src/components/Column.jsx
+++ frontend/src/components/Column.jsx
@@ -2,7 +2,7 @@ import PatientCard from "./PatientCard"
 
 const Column = ({ data, setPatientData }) => {
     return (
-        <div className="flex flex-col space-y-2 overflow-y-scroll">
+        <div className="flex flex-col space-y-2 overflow-y-scroll rounded border border-purple-800">
             {data?.map((item, index) => (
                 <PatientCard
                     data={item}
